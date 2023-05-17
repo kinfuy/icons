55f66a929570eca0003d746c5c909738ec89e410
kinfuy <yonghu520@outlook.com>
2023-05-17 23:27:50 +0800

    docs: 新增copy

--- document/src/App.vue
+++ document/src/App.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
 import * as icons from '../../dist';
 import '../../dist/style/index.css';
+import { ref } from 'vue';
 
 const { KIcon, ...iconlist } = icons;
+
+const copy = ref('');
+const handleCopy = (name: string) => {
+  copy.value = name;
+  navigator.clipboard?.writeText(`<${name}>`);
+  setTimeout(() => {
+    copy.value = '';
+  }, 3000);
+};
 </script>
 
 <template>
   <div class="title">KINFUY ICONS</div>
   <div class="icons-warper">
-    <div class="icon-item" v-for="icon in iconlist" :key="icon.name">
-      <KIcon :size="40">
+    <div
+      class="icon-item"
+      @click="handleCopy(icon.name)"
+      v-for="icon in iconlist"
+      :key="icon.name"
+    >
+      <KIcon :size="40" :color="copy === icon.name ? 'green' : '#333'">
         <component :is="icon" />
       </KIcon>
-      <div class="icon-text">{{ icon.name }}</div>
+      <div
+        class="icon-text"
+        :style="{ color: copy === icon.name ? 'green' : '#333' }"
+      >
+        {{ copy === icon.name ? 'copyed!' : icon.name }}
+      </div>
     </div>
   </div>
 </template>
